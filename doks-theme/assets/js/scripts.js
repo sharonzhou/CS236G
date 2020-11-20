fe4a8f5cd8ce29eb5e507a03af88e1b2aeba7e89
szalouk <sofian.zalouk@mail.utoronto.ca>
2020-11-19 21:28:20 -0800

    Fixed navigation bug

--- doks-theme/assets/js/scripts.js
+++ doks-theme/assets/js/scripts.js
@@ -151,8 +151,6 @@
 		$( '.js-affix' ).affix( {
 			offset: {
 				top: function() {
-					console.log($( '.hero-subheader' ).outerHeight( true ))
-					console.log($( '.frontpage-gif' ).outerHeight( true ))
 					return (
 						this.top = $( '.hero-subheader' ).outerHeight( true ) + $( '.frontpage-gif' ).outerHeight( true )
 					);
